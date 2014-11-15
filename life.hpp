3e90fd4c0dde046f7d81c79a54987caa7c1fac30
贺鹏飞 <hepengfei@xunlei.com>
2014-11-15 11:07:54 +0800

    定义好接口

---
+++ life.hpp
@@ -0,0 +1,21 @@
+
+
+class Grid
+{
+public:
+    void init(int x, int y);    // all dead
+    void setAlive(int x, int y);
+    bool isAlive(int x, int y) const;
+    int numberOfAliveAround(int x, int y) const;
+
+private:
+    int _x;
+    int _y;
+    char _grid;
+};
+
+void nextGeneration(const Grid &prev,
+                    Grid &next);
+
+
+

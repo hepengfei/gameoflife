c0ae710eab5b049e70b51218e3c956a0bd271742
贺鹏飞 <hepengfei@xunlei.com>
2014-11-15 11:21:57 +0800

    完成空的实现，让测试失败

---
+++ life.cpp
@@ -0,0 +1,26 @@
+
+#include "life.hpp"
+
+void Grid::init(int x, int y)
+{
+}
+
+void Grid::setAlive(int x, int y)
+{
+}
+
+bool Grid::isAlive(int x, int y) const
+{
+    return false;
+}
+
+int Grid::numberOfAliveAround(int x, int y) const
+{
+    return 0;
+}
+
+void nextGeneration(const Grid &prev,
+                    Grid &next)
+{
+}
+
